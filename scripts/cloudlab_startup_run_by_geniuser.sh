e285d003927f8c466a7b2651dd5ed82ac28740c4
Jiawei "Tyler" Gu <47795840+tylergu@users.noreply.github.com>
2024-04-18 23:53:05 -0500

    Update cloudlab_startup_run_by_geniuser.sh

--- scripts/cloudlab_startup_run_by_geniuser.sh
+++ scripts/cloudlab_startup_run_by_geniuser.sh
@@ -2,7 +2,7 @@
 if [ ! -f /etc/cloudlab/SETUP_SUCCESS ]
 then
     experiment_creator=$( geni-get user_urn | rev | cut -d '+' -f -1 | rev )
-    sudo su -c /local/repository/scripts/cloudlab_startup_run_by_creator.sh $experiment_creator &> /tmp/startup_log &&\
+    sudo su -c "bash /local/repository/scripts/cloudlab_startup_run_by_creator.sh" $experiment_creator &> /tmp/startup_log &&\
         sudo mkdir /etc/cloudlab &&\
         sudo touch /etc/cloudlab/SETUP_SUCCESS
 fi
